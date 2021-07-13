61d7d01a06928bd549ff271e8a951aeca1af37f0
poonam2096740 <85899531+poonam2096740@users.noreply.github.com>
2021-07-13 21:12:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -30,24 +30,19 @@ var last_position_of_x, last_position_of_y;
         ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
         ctx.stroke();
         }
-
         last_position_of_x = current_position_of_mouse_x; 
         last_position_of_y = current_position_of_mouse_y;
     }
-
     canvas.addEventListener("mouseup", my_mouseup);
     function my_mouseup(e)
     {
         mouseEvent = "mouseUP";
     }
-
     canvas.addEventListener("mouseleave", my_mouseleave);
     function my_mouseleave(e)
     {
         mouseEvent = "mouseleave";
     }
-
-
     function clearArea() 
     {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
